8f8cd62014deac046990370795b3ae82d0eb672d
Pikassik <pikacco.ya@yandex.ru>
2020-10-09 22:44:50 +0300

    removed comments

--- asm_instrs.h
+++ asm_instrs.h
@@ -1,24 +1,5 @@
 #pragma once
 
-//#define inb(port, input) { \
-//  asm volatile ( \
-//      "inb " ## #port ##"%1, %%al\n\t" \
-//      "movb %0, %%al\n\t" \
-//    : "=r" (input) \
-//    : \
-//    : "%eax" \
-//  ); \
-//}
-//
-//#define outb(port, value) { \
-//  asm volatile ( \
-//  "outb %0, $"## #port ##"\n\t" \
-//    : \
-//    : \
-//    : \
-//  ); \
-//}
-
 inline void cli() {
   asm volatile ("cli\n\t");
 }
